5e7ed2f15391e6dccb6c4dddbbad4272cbd25af9
Sanjoy Mahata <108229221+Sanjoy08@users.noreply.github.com>
2022-07-21 12:15:19 +0530

    commit

---
+++ stringliteral.java
@@ -0,0 +1,26 @@
+class A
+{
+public static void main(String args[])
+{
+String a=new String("sanjoy");
+System.out.println(a);
+
+
+String b=new String("mahata");
+System.out.println(b);
+
+
+String C=new String("chandan");
+System.out.println(C);
+
+
+   b=b.concat("sanjoy");
+
+  System.out.println(b);
+            
+
+
+
+
+    }
+    }
